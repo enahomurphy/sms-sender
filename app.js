fa31f17a597b5e03828913beb5937610018de593
Enaho Murphy <enahomurphy@gmail.com>
2018-03-29 01:44:26 +0100

    Add integration testing and coverage config

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.get('/', (req, res) => {
 app.post('/api/v1/send', sendSms)
 
 const port = process.env.PORT || 8081
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app is running on port ${port}`)
 })
 
-module.exports = app
+module.exports = server
