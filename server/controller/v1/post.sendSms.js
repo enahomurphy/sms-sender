2971e5d220811d9ab7dd7742d38eac1410af3a48
Enaho Murphy <enahomurphy@gmail.com>
2018-03-29 01:12:45 +0100

    Rewrite validation for send sms endpoint

--- server/controller/v1/post.sendSms.js
+++ server/controller/v1/post.sendSms.js
@@ -3,13 +3,40 @@ const rp = require('request-promise')
 const infoBip = require('../../config/infobip')
 
 const isString = (val) => val && val.length > 1  || false
-const isPhone = (val) => val && !isNaN(val) && val.toString().length > 7 || false
+const isPhone = (val) => val &&
+  !isNaN(val) &&
+  val.toString().length > 7 ||
+  false
 
-const validateRequest = ({message, phone, name}) => {
-  if (!isString(message) || !isPhone(phone) || !isString(name)) {
+// validates request and generates error messages
+// for each fields
+const validateRequest = (body) => {
+  const errors = []
+  const RequestKeys = Object.keys(body)
+  const validFields = ['phone', 'name', 'message']
+
+  validFields.forEach((key) => {
+    const validFields = RequestKeys.includes(key)
+    if ((validFields && key === 'phone') && !isPhone(body[key])) {
+      errors.push({
+        path: key,
+        message: `${key} is required and must be a valid number`
+      })
+    }
+    else if ((!validFields || !isString(body[key])) ||
+      (validFields && !isString(body[key]))  ) {
+        errors.push({
+          path: key,
+          message: `${key} is required and must be a valid ${key}`
+        })
+    }
+  })
+
+  if (errors.length) {
     throw({
       status: 422,
-      message: "All fields are required to send a sms"
+      errors,
+      message: "All fields are required and must be valid"
     })
   }
 }
@@ -20,9 +47,9 @@ const sendMessage = (body) => {
     .toString('base64')
 
   const infoBipBody = {
-    "from": body.name,
-    "to": body.phone,
-    "text": body.message
+    from: body.name,
+    to: body.phone,
+    text: body.message
   }
   const headers = {
     'Content-Type': 'Application/json',
@@ -49,11 +76,13 @@ const handler = async (req, res) => {
       .status(200)
       .send({ message: `message sent to ${req.body.phone}`}) 
   } catch(err) {
-    console.log(err)
     if (err.status) {
       return res
         .status(err.status)
-        .send({ message: err.message})
+        .send({ 
+          message: err.message,
+          errors: err.errors
+        })
     }
     res
       .status(500)
